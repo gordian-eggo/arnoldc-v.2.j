b29371068af09595636d64af741df2f54986b90a
gordian-eggo <columbus.knot@gmail.com>
2018-11-28 18:28:13 +0800

    First commit. Includes current project state as of 11-22-18.

---
+++ ArnoldC-interpreter/ArnoldC-interpreter/Program.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using Gtk;
+
+// namespace ArnoldCinterpreter
+// {
+//     class MainClass
+//     {
+//         public static void Main(string[] args)
+//         {
+//             Application.Init();
+//             MainWindow win = new MainWindow();
+//             win.Show();
+//             Application.Run();
+//         }
+//     }
+// }
+
+namespace ArnoldCinterpreter {
+
+    class MainClass {
+
+        static void Main(string[] args) {
+
+            Application.Init();
+            MainWindow win = new MainWindow();
+            win.Show();
+            Application.Run();
+
+            StreamReader arnoldc_reader = new StreamReader("../../lexemes.arnoldc");
+            string data_string = arnoldc_reader.ReadLine();
+            var symbol_table = new Dictionary<int, Tuple<string, string>>();
+            int i = 1;          // fuse this later as an index for putting lexemes into symbol_table
+
+            // these are the regexes for matching later on. I categorized them by usage so they're easier to keep 
+            // track of and to avoid one massive block of code
+            Regex keyword_regex = new Regex(@"\bIT'S SHOWTIME\b|\bYOU HAVE BEEN TERMINATED\b|\bTALK TO THE HAND\b
+                                |\bI LET HIM GO\b|\bLISTEN TO ME VERY CAREFULLY\b|\bGIVE THESE PEOPLE AIR\b
+                                |\bI NEED YOUR CLOTHES YOUR BOOTS AND YOUR MOTORCYCLE\b
+                                |\bI'LL BE BACK\b|\bHASTA LA VISTA, BABY\b|DO IT NOW\b|\bGET YOUR ASS TO MARS\b
+                                |\bI WANT TO ASK YOU A BUNCH OF QUESTIONS AND I WANT TO HAVE THEM ANSWERED IMMEDIATELY\b
+                                |\bWHAT THE FUCK DID I DO WRONG\b");
+
+            Regex reassign_variable = new Regex(@"\bGET TO THE CHOPPER\b|HERE IS MY INVITATION\b|\bENOUGH TALK\b");
+            Regex assign_variable = new Regex(@"\bHEY CHRISTMAS TREE\b|YOU SET US UP\b");
+            Regex arithmetic_operations_regex = new Regex(@"\bGET DOWN\b|\bGET UP\b|
+                                |\bYOU'RE FIRED\b|\bHE HAD TO SPLIT\b");
+
+            Regex boolean_ops_regex = new Regex(@"\bLET OFF SOME STEAM BENNET\b|\bYOU ARE NOT YOU YOU ARE ME\b
+                                |\bCONSIDER THAT A DIVORCE\b|\bKNOCK KNOCK\b");
+
+            Regex print_regex = new Regex(@"\bTALK TO THE HAND\b");
+
+            Regex if_loop_regex =new Regex(@"\bBECAUSE I'M GOING TO SAY PLEASE\b");
+            Regex else_if_loop_regex = new Regex(@"\bBULLSHIT\b|\bYOU HAVE NO RESPECT FOR LOGIC\b");
+
+            Regex macro_regex = new Regex(@"\bI LIED\b|\bNO PROBLEMO\b");
+            Regex while_regex = new Regex(@"\bSTICK AROUND\b");
+            Regex end_while_regex = new Regex(@"\bCHILL\b");
+            Regex integer_regex = new Regex(@"\b\d+\b");
+            Regex variable_name_regex = new Regex(@"([a-zA-Z0-9]*[a-z0-9]+)");
+
+            while (data_string != null) {       // iterates through the strings in the file
+
+                MatchCollection string_literals = Regex.Matches(data_string, @"""(.*?)""");
+
+                Match keyword_match = keyword_regex.Match(data_string);
+                Match rv_match = reassign_variable.Match(data_string);
+                Match av_match = assign_variable.Match(data_string);
+                Match math_ops_match = arithmetic_operations_regex.Match(data_string);
+                Match bool_ops_match = boolean_ops_regex.Match(data_string);
+
+                Match if_statement_match = if_loop_regex.Match(data_string);
+                Match else_if_match = else_if_loop_regex.Match(data_string);
+
+                Match while_match = while_regex.Match(data_string);
+                Match end_while_match = end_while_regex.Match(data_string);
+
+                Match print_match = print_regex.Match(data_string);
+                Match integer_match = integer_regex.Match(data_string);
+                Match variable_name_match = variable_name_regex.Match(data_string);
+                Match macro_match = macro_regex.Match(data_string);
+
+                if (end_while_match.Success) {      // separated CHILL from STICK AROUND so if a user decides to put anything else
+                                                    // after CHILL it won't be read            
+                    var end_while_token = Tuple.Create("Keyword", end_while_match.Value);
+                    symbol_table.Add(i, end_while_token);
+                    i = i + 1;
+
+                }
+
+                if (while_match.Success) {                  // matches lexemes inside while loops
+
+                    var token_input = Tuple.Create("Keyword", while_match.Value);
+                    symbol_table.Add(i, token_input);
+                    i = i + 1;
+
+                    if (variable_name_match.Success) {      // put this here to catch the variable name after STICK AROUND
+
+                        var variable_token = Tuple.Create("Variable name", variable_name_match.Value);
+                        symbol_table.Add(i, variable_token);
+                        i = i + 1;
+
+                    }
+
+                }
+
+                if (keyword_match.Success) {        // for the other uncategorized keywords
+
+                    var token_input = Tuple.Create("Keyword", keyword_match.Value);
+                    symbol_table.Add(i, token_input);
+                    i = i + 1;
+
+                }
+
+                if (print_match.Success) {      // same implementation as the while functionality. It's also used for the other functionalities.
+
+                    var print_token = Tuple.Create("Keyword", print_match.Value);
+                    symbol_table.Add(i, print_token);
+                    i = i + 1;
+
+                    
+                    if (integer_match.Success) {
+
+                        var integer_token = Tuple.Create("Integer literal", integer_match.Value);
+                        symbol_table.Add(i, integer_token);
+                        i = i + 1;
+
+                    } else if (variable_name_match.Success) {
+
+                        var variable_token = Tuple.Create("Variable name", variable_name_match.Value);
+                        symbol_table.Add(i, variable_token);
+                        i = i + 1;
+
+                    } else if (macro_match.Success) {
+
+                        var macro_token = Tuple.Create("False macro", macro_match.Value);
+                        symbol_table.Add(i, macro_token);
+                        i = i + 1;
+
+                    }
+
+                }
+
+                if (av_match.Success) {
+
+                    var token_input = Tuple.Create("Keyword", av_match.Value);
+                    symbol_table.Add(i, token_input);
+                    i = i + 1;
+
+                    if (integer_match.Success) {
+                        var integer_token = Tuple.Create("Integer literal", integer_match.Value);
+                        symbol_table.Add(i, integer_token);
+                        i = i + 1;
+                    } else if (variable_name_match.Success) {
+                        var variable_token = Tuple.Create("Variable name", variable_name_match.Value);
+                        symbol_table.Add(i, variable_token);
+                        i = i + 1;
+                    } else if (macro_match.Success) {
+                        var macro_token = Tuple.Create("False macro", macro_match.Value);
+                        symbol_table.Add(i, macro_token);
+                        i = i + 1;
+                    }
+
+                }
+
+
+                if (rv_match.Success) {
+
+                    var token_input = Tuple.Create("Keyword", rv_match.Value);
+                    symbol_table.Add(i, token_input);
+                    i = i + 1;
+
+                    if (integer_match.Success) {
+
+                        var integer_token = Tuple.Create("Integer literal", integer_match.Value);
+                        symbol_table.Add(i, integer_token);
+                        i = i + 1;
+
+                    } else if (variable_name_match.Success) {
+
+                        var variable_token = Tuple.Create("Variable name", variable_name_match.Value);
+                        symbol_table.Add(i, variable_token);
+                        i = i + 1;
+
+                    } else if (macro_match.Success) {
+
+                        var macro_token = Tuple.Create("False macro", macro_match.Value);
+                        symbol_table.Add(i, macro_token);
+                        i = i + 1;
+
+                    }                  
+
+                }
+
+                if (bool_ops_match.Success) {
+                    var bool_input = Tuple.Create("Keyword", bool_ops_match.Value);
+                    symbol_table.Add(i, bool_input);
+                    i = i + 1;
+
+
+                    if (integer_match.Success) {
+
+                        var integer_token = Tuple.Create("Integer literal", integer_match.Value);
+                        symbol_table.Add(i, integer_token);
+                        i = i + 1;
+
+                    } else if (variable_name_match.Success) {
+
+                        var variable_token = Tuple.Create("Variable name", variable_name_match.Value);
+                        symbol_table.Add(i, variable_token);
+                        i = i + 1;
+
+                    }
+                    
+                }
+
+                if (math_ops_match.Success) {
+
+                    var token_input = Tuple.Create("Keyword", math_ops_match.Value);
+                    symbol_table.Add(i, token_input);
+                    i = i + 1;
+
+                    if (integer_match.Success) {
+
+                        var integer_token = Tuple.Create("Integer literal", integer_match.Value);
+                        symbol_table.Add(i, integer_token);
+                        i = i + 1;
+
+                    } else if (variable_name_match.Success) {
+
+                        var variable_token = Tuple.Create("Variable name", variable_name_match.Value);
+                        symbol_table.Add(i, variable_token);
+                        i = i + 1;
+
+                    }
+
+                }
+
+                if (if_statement_match.Success) {
+
+                    var token_input = Tuple.Create("Keyword", if_statement_match.Value);
+                    symbol_table.Add(i, token_input);
+                    i = i + 1;
+
+                    if (integer_match.Success) {
+
+                        var integer_token = Tuple.Create("Integer literal", integer_match.Value);
+                        symbol_table.Add(i, integer_token);
+                        i = i + 1;
+
+                    } else if (variable_name_match.Success) {
+
+                        var variable_token = Tuple.Create("Variable name", variable_name_match.Value);
+                        symbol_table.Add(i, variable_token);
+                        i = i + 1;
+
+                    }
+
+                }
+
+                if (else_if_match.Success) {
+                    var token_input = Tuple.Create("Keyword", else_if_match.Value);
+                    symbol_table.Add(i, token_input);
+                    i = i + 1;
+                }
+
+                // keeping this as a template for now
+                // if (.Success) {
+                //     var token_input = Tuple.Create("Keyword", .Value);
+                //     symbol_table.Add(i, token_input);
+                //     i = i + 1;
+                // }
+                
+
+                foreach (Match string_literal in string_literals) {         // catches string literals
+                    var token_input = Tuple.Create("String literal", string_literal.Groups[1].Value);
+                    symbol_table.Add(i, token_input);
+                    i = i + 1;
+                }
+
+                data_string = arnoldc_reader.ReadLine();
+            }
+
+            foreach (KeyValuePair<int, Tuple<string, string>> item in symbol_table) {
+                Console.WriteLine("Key: {0}, Value: {1}", item.Key, item.Value);
+            }
+
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,88 @@
+NOTES FOR CMSC 124 ARNOLDC INTERPRETER:
+
+ArnoldC.jar - the ArnoldC language pack for reference. Usage: ArnoldC [-run|-declaim] [FileToSourceCode]
+			- pag na-run ko siya nang maayos update ko kayo via readme/messenger haha
+
+11-14-18, 2014H: caught exception for MatchCollections on lines 40 and 41. Parang di pa niya kaya basahin yung 
+				 newline character kaya null yung nirereturn ng match(). Di ko pa sure kung paano ayusin.
+
+		  2031H: fixed contents of Program.cs, nadoble yung laman. Inayos din ibang variable names for readability and added regex for 		   			   variables.	
+
+		  2252H: add variable regex and changed variable names for readability. 
+
+11-15-18, 0229H: took the working parts for variable and function names from Waldo's code. Not a merge, more of an update.
+  approx. 0317H: added better matches for macros and and integers. Integers can now be read next to the keywords YOU SET US UP, GET UP, and GET DOWN.
+  		  0748H: encountered exceptions at lines 43 and 72, not sure how to fix it so ask Ma'am. Short description below. 
+  		  		 Line 43: System.ArgumentException has been thrown parsing the contents of line 41.
+  		  		 Line 72: System.ArgumentNullException has been thrown, value cannot be null.
+  		  0801H: possible cause for bug found. The while loop that iterates through the document while data = sr.ReadLine() 
+  		  		 is not null stops at YOU HAVE BEEN TERMINATED without having analyzed the line.
+
+11-18-18, 1301H: running sample.arnoldc for reference while making the state diagram. Comments in section below.
+
+		  1437H: finished making sample programs for reference re: ArnoldC syntax. 
+		         To run, use the command: clear && java -jar ArnoldC.jar <filename>.arnoldc && java <filename>
+
+11-21-18, 1737H: checked Waldo's code for lexical analyzer using C#'s dictionary. Bugs include:
+
+				 > string literals are read as string literals and identifiers, e.g. "hello" is saved in the dictionary as a string literal and an identifier. Same thing happens for integer literals.
+
+				 > variables are mistakenly saved as just identifiers. They have to be saved as "variable names"/"variable identifiers" to avoid confusion during syntax analysis (Fully fixed as of 2343H)
+
+				 > variable names are allowed to have capital letters, ergo the variable ABC in lexemes.arnoldc should be read and saved as a variable (Fully fixed as of 2343H)
+
+				 NOTE: variable names cannot have special characters like _/&/%/$ and so on. This counts as a bug, but the fix is a bit more advanced than we have time for at the moment.
+
+		  1957H: falling out with Waldo is the last straw in a haystack of issues so will resume code alone. Adjusted variable names for  			   readability again, for the last time.
+		  2343H: Fixed the bug where variable names are allowed to have capital letters. Due to time constraints, I might have to assume that 		   the user will adopt a more or less conventional variable naming scheme far from the likes of h4xx0r style.
+
+		  2352H: Partially fixed the bug where integer literals are read as integer literals and identifiers. Moved the conditional loops for 		   it into pre-existing keyword conditional loops.
+
+		  0007H: Encountered another bug. The fix for the first bug where you move conditional loops into the pre-existing keyword loops result 	   in the keyword being written to the dictionary twice. (Fixed as of 0009H)
+
+		  0037H: Having trouble getting the regex for the macros correct. The @ character isn't getting read. But I will not lose hope, for my 		   roommate bought me McDonalds. Additionally, it might need the use of the "\p{}" regex using the Unicode value for @
+
+11-22-18, 1009H: fixed while loop regexes such that the contents can be added to the dictionary of lexemes.
+
+		  1021H: error pushing updated files to my branch. Saved a copy in a different location. (Fixed as of 1051H)
+
+		  1119H: added functionality for boolean operations.
+
+		  1227H: Finally adjusted project for GUI. Original files were somehow made in a different way from what was taught in lab so I moved 		   the contents of the original files into the new Gtk project. Will create a separate repo for this.
+
+		  1240H: holy [deleted] naksimula ako ng GUI I feel the power of technology [deleted]
+
+		  1403H: Monodevelop bug prevents me from progressing further on UI for now so will continue lexical and start on syntax as best as I 		   can. Bandaided the false macro bug, removed @ for the time being but will bring up the issue with ma'am. Presentation has been 		 moved to 1510H.
+
+		  1408H: Closer inspection of the project specifications state that I've covered the required features. I am delighted. Lexical 			   analyzer officialy finished at 1408H.
+
+		  1448H: Lexical analyzer is buggy. I cry. Bugs include:
+
+		  		 > part of some strings are read as variable names
+
+		  		 > adjusting else-if regexes result in the else-if keywords BULLSHIT and YOU HAVE NO RESPECT FOR LOGIC being put in the 		  dictionary twice. (Fixed as of 1453H)
+
+		  		 > BECAUSE I'M GOING TO SAY PLEASE isn't being read. (Fixed as of 1453H)
+
+
+
+COMMENTS ON SYNTAX:
+
+	> Variable names with underscores and possibly other special characters are not allowed.
+	> YOU SET US UP doesn't work with string variables
+	> free-form positioning
+	> the GET TO THE CHOPPER loop completely erases and replaces the value in a variable with a new value defined inside the loop
+	> if you do a comparison inside the GET TO THE CHOPPER loop, for example: "temp = 4 > 5" and then print it, it will print either 1(true) or 0(false)
+	> if statements are harder, will need more time to make a suitable state diagram for them
+	> structure for if-else statement:
+		1. initialize a variable with a value
+		2. (optional) GET TO THE CHOPPER loop
+		3. if/if-else statement
+	> if the value on BECAUSE I'M GOING TO SAY PLEASE is greater than 0, it will execute the if-statement
+	> if there's no GET TO THE CHOPPER loop, it will automagically execute the if-statement and ignore the "else" art if there is one
+	> the value of the condition statement has to be calculated beforehand
+	> initialize all variables that you're going to use
+	> you have to compare the value for the while loop to a different value in a different variable
+	> you can't put a GET TO THE CHOPPER loop and/or a HEY CHRISTMAS TREE variable declaration inside a previous GET TO THE CHOPPER loop
+	> you can't put a print statement between HEY CHRISTMAS TREE and YOU SET US UP
+	> you can't put if statements/if-else statements inside a GET TO THE CHOPPER loop
